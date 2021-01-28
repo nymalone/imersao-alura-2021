9ca3f2da0be339fb1549b454c4aa5b0b8f7a8894
Nykolle Malone <malone.nykolle@gmail.com>
2021-01-28 14:33:05 -0300

    firsts containers created

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
 import styled from 'styled-components'
+import db from "../db.json";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  background-size: cover;
+  background-position: center;
+  flex: 1;
+`;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <BackgroundImage>
+      lalala
+    </BackgroundImage>
+  )
 }
